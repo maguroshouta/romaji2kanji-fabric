90c2c7fe01ee20bbe8b6e61bc90aab7d86389731
yakimaguro <maguroshouta@gmail.com>
2025-11-13 15:19:27 +0900

    変換対象を半角英数字記号のみの入力に限定

--- src/main/java/com/yakimaguro/romaji2kanji/RomajiMod.java
+++ src/main/java/com/yakimaguro/romaji2kanji/RomajiMod.java
@@ -7,15 +7,24 @@ import net.fabricmc.fabric.api.message.v1.ServerMessageDecoratorEvent;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 
+import java.util.regex.Pattern;
+
 public class RomajiMod implements ModInitializer {
+
+    private static final Pattern ASCII_ONLY = Pattern.compile("^\\p{ASCII}+$");
 
     @Override
     public void onInitialize() {
         ServerMessageDecoratorEvent.EVENT.register(ServerMessageDecoratorEvent.CONTENT_PHASE,
                 (ServerPlayerEntity sender, Text message) -> {
                     String original = message.getString();
-                    String converted = IMEConverter.convByGoogleIME(YukiKanaConverter.conv(original));
-                    return Text.literal(original + " -> " + converted);
-                });
+
+                    if (ASCII_ONLY.matcher(original).matches()) {
+                        String converted = IMEConverter.convByGoogleIME(YukiKanaConverter.conv(original));
+                        return Text.literal(original + " -> " + converted);
+                    } else {
+                        return message;
+                    }
+        });
     }
 }
